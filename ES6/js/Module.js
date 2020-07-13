e5aa8cdaefee47f8e30f7d9cd63fe758959f7ed8
dxy <dxy2020mailbox@163.com>
2020-07-13 11:48:36 +0800

    20200713整理

--- ES6/js/Module.js
+++
@@ -1,5 +0,0 @@
-import {a} from './moduleout.js';
-console.log(a);
-//const isNotModuleScript = this !== undefined;
-//console.log(isNotModuleScript);
-setTimeout(()=>console.log(a),2000)
--- ES6/js/moduleout.js
+++
@@ -1,4 +0,0 @@
-export let a='您好！';
-setTimeout(()=>{
-	a='我是张三';
-},2000);

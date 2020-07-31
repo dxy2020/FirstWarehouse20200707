80d373c28915dce9f9318fc961b9ce3005360f96
dxy2020 <dxy2020mailbox@163.com>
2020-07-31 20:06:46 +0800

    面包屑组件

---
+++ beginproject/myvue/src/components/custom-breadcrumb/CustomBreadcrumb.vue
@@ -0,0 +1,56 @@
+<template>
+    <div id="breadcrumb">
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+            <el-breadcrumb-item v-for="(values,index) in breadcrumb_item" :key="index" :to="{path:values.topath}">
+                {{ values.content }}
+            </el-breadcrumb-item>
+        </el-breadcrumb>
+        <i class="el-icon-arrow-left" style="font-size: 40px;" @click="isOpenThirdMenu" />
+    </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      // breadcrumb_item_data:{},
+      breadcrumb_item:[
+        {
+          'content':'首页',
+          'topath':'/'
+        },
+        {
+          'content':'规划服务',
+          'topath':'#'
+        }
+      ]
+    };
+  },
+  watch:{
+    $route:{
+      handler:function(val,oldVal){
+        console.log(val.name,oldVal.path);
+      }
+    }
+  },
+  methods:{
+    isOpenThirdMenu(){
+      this.$store.commit('openleftmenu02');
+    }
+			
+  }
+};
+</script>
+
+<style scoped lang="scss">
+  #breadcrumb{
+      width: auto;
+      display: inline-flex;
+      justify-content: space-between;
+      margin: {
+        top: 12px;
+        bottom:24px;
+      }
+  }
+
+</style>

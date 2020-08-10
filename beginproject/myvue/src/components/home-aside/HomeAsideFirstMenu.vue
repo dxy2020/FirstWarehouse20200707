b2d24f379c7dbd038ab836ee6f5d32dedb6d57bc
dxy2020 <dxy2020mailbox@163.com>
2020-08-10 20:32:23 +0800

    隐藏文字

--- beginproject/myvue/src/components/home-aside/HomeAsideFirstMenu.vue
+++ beginproject/myvue/src/components/home-aside/HomeAsideFirstMenu.vue
@@ -1,16 +1,18 @@
 <template>
-    <div id="home-aside-first-menu">
-        <img src="./img/u24.png">
+    <div id="home-aside-first-menu" :style="{width:first_menu_width+'px'}">
+        <img :src="top_img_switch?top_img_src01:top_img_src02" @click="removeDescription">
         <div
             v-for="(value,index) in menu" :key="index" class="menu-button"
-            @click="isOpenSecondMenu"
+            @click="isOpenSecondMenu(index)"
         >
             <router-link
-                :to="value.tolink" tag="div" append
+                :to="{path:value.tolink,query:value.query}" tag="div" append
                 style="cursor:pointer"
             >
                 <img :src="value.imgsrc" style="width: 35%;">
-                <p><span>{{ value.menucontent }}</span></p>
+                <p v-show="isShowDescription">
+                    <span>{{ value.menucontent }}</span>
+                </p>
             </router-link>
         </div>
     </div>
@@ -21,29 +23,50 @@ export default {
   name: 'HomeAsideFirstMenu',
   data() {
     return {
+      first_menu_width:60,
+      top_img_switch:true,
+      top_img_src01:require('./img/u24.png'),
+      top_img_src02:require('./img/u65.png'),
+      clickcount:0,
+      isShowDescription:true,
       menu: [
         {
-          tolink: '#',
+          tolink: '/home/homeasidesecondmenu',
+          query:{
+            secondmenu_id:0
+          },
           imgsrc: require('./img/u69.png'), // 此时需要用require，否则图片无法加载
           menucontent: '通用功能'
         },
         {
-          tolink: '/home/planservices',
+          tolink: '/home/homeasidesecondmenu',
+          query:{
+            secondmenu_id:1
+          },
           imgsrc: require('./img/u72.png'),
           menucontent: '规划服务'
         },
         {
-          tolink: '#',
+          tolink: '/home/homeasidesecondmenu',
+          query:{
+            secondmenu_id:2
+          },
           imgsrc: require('./img/u79.png'),
-          menucontent: '监管服务'
+          menucontent: '审批服务'
         },
         {
-          tolink: '#',
+          tolink: '/home/homeasidesecondmenu',
+          query:{
+            secondmenu_id:3
+          },
           imgsrc: require('./img/u65.png'),
           menucontent: '监管服务'
         },
         {
-          tolink: '#',
+          tolink: '/home/homeasidesecondmenu',
+          query:{
+            secondmenu_id:4
+          },
           imgsrc: require('./img/u67.png'),
           menucontent: '决策服务'
         }
@@ -52,8 +75,22 @@ export default {
     };
   },
   methods: {
-    isOpenSecondMenu() {
-      this.$store.commit('OpenSecondMenu');
+    isOpenSecondMenu(index) {
+      if(this.$store.state.openSecondMenu===false){
+        this.$store.commit('OpenSecondMenu');
+        this.$store.commit('secondMenuSelect',index);
+      }else{
+        if(index===this.$store.state.secondMenuSelect){
+          this.$store.commit('OpenSecondMenu');
+        }else{
+          this.$store.commit('secondMenuSelect',index);
+        }
+      };      
+    },
+    removeDescription(){
+      this.top_img_switch=!this.top_img_switch;
+      this.isShowDescription=!this.isShowDescription;      
+      this.first_menu_width=this.top_img_switch?60:40;   
     }
   }
 };
@@ -62,7 +99,6 @@ export default {
 <style scoped="scoped" lang="scss">
 	#home-aside-first-menu{
     height: 100%;
-		width: 60px;		
 		background-color: #505458;
 		text-align: center;
     color: #FFFFFF;
@@ -71,6 +107,7 @@ export default {
     img{
       width: 45%;
       padding-top: 8px;
+      cursor:pointer;
     }
     .menu-button{
       font-size: 12px;
